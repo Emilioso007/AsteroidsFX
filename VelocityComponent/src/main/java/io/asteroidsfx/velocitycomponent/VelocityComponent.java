c0869768aafe7f779edd569e942a83fd68e8c077
Emil Jørgensen <emiljorgensen05@gmail.com>
2026-02-21 16:52:55 +0100

    add VelocityComponent module

---
+++ VelocityComponent/src/main/java/io/asteroidsfx/velocitycomponent/VelocityComponent.java
@@ -0,0 +1,7 @@
+package io.asteroidsfx.velocitycomponent;
+
+import io.asteroidsfx.common.Component;
+
+public class VelocityComponent extends Component{
+    public double dx, dy;
+}
---
+++ VelocityComponent/src/main/java/module-info.java
@@ -0,0 +1,3 @@
+module VelocityComponent {
+    requires Common;
+}
