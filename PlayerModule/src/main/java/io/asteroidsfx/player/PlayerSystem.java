cc0c56aea34a8f6ed256205a03502019a9d4338f
Emil Jørgensen <emiljorgensen05@gmail.com>
2026-03-02 20:41:46 +0100

    extract variables for clarity

--- PlayerModule/src/main/java/io/asteroidsfx/player/PlayerSystem.java
+++ PlayerModule/src/main/java/io/asteroidsfx/player/PlayerSystem.java
@@ -51,7 +51,8 @@ public class PlayerSystem extends IntervalIteratingSystem {
             case UP, W:
                 AccelerationComponent acceleration = player.getComponent(AccelerationComponent.class);
                 double angle = player.getComponent(AngleComponent.class).angle;
-                acceleration.acc.add(Vector.fromAngle(angle).setMag(2500));
+                int force = 2500;
+                acceleration.acc.add(Vector.fromAngle(angle).setMag(force));
                 break;
         }
     }
@@ -75,7 +76,12 @@ public class PlayerSystem extends IntervalIteratingSystem {
     public void updateInterval(BaseEntity player, double deltaTime) {
         PositionComponent position = player.getComponent(PositionComponent.class);
         AngleComponent angle = player.getComponent(AngleComponent.class);
-        BulletEntity bullet = new BulletEntity(player, position.pos.copy().add(Vector.fromAngle(angle.angle).setMag(60)), Vector.fromAngle(angle.angle).setMag(600));
+
+        Vector startPosition = position.pos.copy().add(Vector.fromAngle(angle.angle).setMag(60)); // bullet spawns 60 pixels in front of the player
+        Vector velocity = Vector.fromAngle(angle.angle).setMag(600);
+
+        BulletEntity bullet = new BulletEntity(player, startPosition, velocity);
+
         SpawnEvent event = new SpawnEvent();
         event.entityToSpawn = bullet;
         World.getInstance().getEventBus().publish(event);
